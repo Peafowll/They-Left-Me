fbc9d0c299437458cceaa174b56a45f2404c9d94
Peafowll <tudorpaun16@gmail.com>
2025-07-21 18:00:11 +0300

    Added player, world, both modes

---
+++ player.gd
@@ -0,0 +1,6 @@
+extends Node2D
+# Player Stats
+@export_range(0,100) var hunger : float = 100
+@export_range(0,100) var thrist : float = 100
+@export_range(0,100) var energy : float = 100
+@export_range(0,100) var sanity : float = 100
---
+++ world.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+@export var day : int = 0
+@export var hour : float = 0
