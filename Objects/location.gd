11dcf1dfa2069970ee37b1a7df75de35e59d4f90
Peafowll <tudorpaun16@gmail.com>
2025-07-22 18:32:14 +0300

    Search Buttons Rudimentary

--- Objects/location.gd
+++ Objects/location.gd
@@ -4,6 +4,6 @@ extends Resource
 @export var background_image : String #replace
 @export var background_noise : String #replace this
 @export var size : int
-@export_range(1,100) var no_loot_chance : int
-@export_range(1,100) var shelter_chance : int
+@export_range(1,100) var no_loot_chance : int # 1-100 (%)
+@export_range(1,100) var shelter_chance : int # 1-100 (%)
 @export var loot_table : LootTable
--- Objects/ui_update.gd
+++ Objects/ui_update.gd
@@ -4,7 +4,30 @@ extends Control
 @onready var thirst_bar: ProgressBar = $ThirstBar
 @onready var hunger_bar: ProgressBar = $HungerBar
 
-func _process(delta: float) -> void:
+@onready var canvas_layer = $"../TravelingTransition"
+
+#@onready var food_search_button : Button = $SearchFoodButton
+#@onready var water_search_button : Button = $SearchWaterButton
+#@onready var shelter_search_button : Button = $SearchShelterButton
+
+
+func _process(_delta: float) -> void:
 	energy_bar.value = Player.energy
 	hunger_bar.value = Player.hunger
 	thirst_bar.value = Player.thirst
+
+func _ready() -> void:
+	canvas_layer.visible = false
+	$SearchFoodButton.pressed.connect(func(): _on_search_button_pressed("food"))
+	$SearchWaterButton.pressed.connect(func(): _on_search_button_pressed("water"))
+	$SearchShelterButton.pressed.connect(func(): _on_search_button_pressed("shelter"))
+
+func _on_travel_button_pressed() -> void:
+	canvas_layer.visible = true
+	# Play footstep sound
+	await get_tree().create_timer(2).timeout
+	canvas_layer.visible = false
+	
+func _on_search_button_pressed(tag : String):
+	print("Cauti " + str(tag))
+	World._search(tag)
