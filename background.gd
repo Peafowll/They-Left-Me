39ef4ec4d426bc6314b20a3b792526f6b996734d
Painite01 <painite01official@gmail.com>
2025-07-22 19:01:01 +0300

    Created textbox + global script for it

--- background.gd
+++ background.gd
@@ -9,7 +9,7 @@ var screen_center: Vector2
 func _ready():
 	original_position = position
 	screen_center = get_viewport().get_visible_rect().size * 0.5
-
+	TextBox.textbox.display_text("Whoa-oh-oh-oh-oh-oh-oh-oh-oh-oh Story of [color=#FF0000]UNDERTALE[/color] I fell from the light Talk? Or should I fight? Monster genocide This my [color=#FF0000]UNDERTALE! [/color]")
 func _process(delta):
 	var mouse_pos = get_global_mouse_position()
 	var _offset = (mouse_pos - screen_center) * parallax_strength
